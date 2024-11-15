494ba129e730325c10c68e252fbf8150fa0fa525
Luca Rogers <124876210+Acul4321@users.noreply.github.com>
2024-11-15 15:14:26 +0000

    api binary setup

--- server/main.ts
+++ server/main.ts
@@ -1,13 +1,23 @@
 export function add(a: number, b: number): number {
   return a + b;
 }
+
+const PORT:string = Deno.env.get("SERVER_PORT") ?? "8000";
+
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-//if (import.meta.main) {} runs every time main is ran
+if (import.meta.main) {
+  console.log("Luca Rogers information api");
+  console.log("Listening on http://localhost:" + PORT + '/')
+}
 
 import { Application, Router } from "@oak/oak";
 import { oakCors } from "@tajpouria/cors";
 
 const router = new Router();
+
+router.get("/", (context) => {
+  context.response.body = {"commands" : "server/hello"}
+})
 
 router.get("/server/hello", (context) => {
   context.response.body = {"message": "hello World"};
@@ -18,4 +28,4 @@ app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 8000 });
+await app.listen({ port: parseInt(PORT) });
